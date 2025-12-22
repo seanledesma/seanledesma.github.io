603195d0bd12eb95598397b3e126566ad9f2776a
seanledesma <seanledesma@users.noreply.github.com>
2025-12-22 22:51:37 +0000

    deploy: 1c98d3717da1be7ac2e13c406577701e04db3448

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765236014',resources: [
+    cacheName: 'chirpy-1766443896',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
